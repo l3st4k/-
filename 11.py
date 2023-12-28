96e86aa8a03634be0feeb0a56ca4995656f0ec0e
Степан Кожеуров <155057219+l3st4k@users.noreply.github.com>
2023-12-28 16:50:07 +0300

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,5 +1,12 @@
-import checker_chisel
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-n = int(input())
-
-print(checker_chisel.check_na_prostotu(n))
+if __name__ == "__main__":
+    user_input = int(input("Введите целое число: "))
+    result = is_prime(user_input)
+    print(f"Является ли число простым: {result}")
