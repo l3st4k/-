3ee46871131e4d0366561375061f08f98d29e67c
Степан Кожеуров <155057219+l3st4k@users.noreply.github.com>
2023-12-28 16:55:44 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,4 +1,21 @@
-import converer_ch_sl
+num = int(input())
+def numeral_word(num):
+    numerals = {
+        1: "один",
+        2: "два",
+        3: "три",
+        4: "четыре",
+        5: "пять",
+        6: "шесть",
+        7: "семь",
+        8: "восемь",
+        9: "девять",
+        10: "десять",
+        11: "одиннадцать",
+        12: "двенадцать"
+    }
+    return numerals.get(num, "")
 
-for i in range(1, 12 + 1):
-    print(i, '=', converer_ch_sl.converter(i))
+if __name__ == "__main__":
+    for i in range(1, 13):
+        print(numeral_word(i))
