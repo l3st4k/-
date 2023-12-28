913ce198dd6e80d6976dbcfdd9a744d97480e56b
l3st4k <155057219+l3st4k@users.noreply.github.com>
2023-12-28 11:08:34 +0300

    Add files via upload

---
+++ Инд 1.py
@@ -0,0 +1,7 @@
+celsius = float(input("Введите температуру в градусах Цельсия: "))
+
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print(f"Температура по шкале Фаренгейта: {fahrenheit} градусов")
+print(f"Температура по шкале Кельвина: {kelvin} градусов")
